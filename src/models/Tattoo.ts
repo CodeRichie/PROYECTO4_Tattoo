d92e6a74e501a8efd103614940f563accfce2426
CodeRichie <ricardartigasartigas@gmail.com>
2024-03-14 11:36:03 +0100

    models fill it

--- src/models/Tattoo.ts
+++ src/models/Tattoo.ts
@@ -1,6 +1,32 @@
-import { Entity } from "typeorm"
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Appointment } from "./Appointment";
 
-@Entity()
-export class Tattoo {
+@Entity("tattoos")
+export class Tattoo extends BaseEntity{
+  @PrimaryGeneratedColumn()
+  id!: number;
 
+  @Column()
+  work!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  photo!: string;
+
+  @Column()
+  price!: number;
+
+  @Column()
+  created_at!: Date;
+
+  @Column()
+  updated_at!: Date;
+
+  @OneToMany(() => Appointment, appointment => appointment.user)
+  appointments!: Appointment[];
 }
